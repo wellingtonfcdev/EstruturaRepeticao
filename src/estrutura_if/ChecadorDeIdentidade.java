5d358d187d595db4835717ef2d0d35f9df4b58f5
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-04-22 16:10:06 -0300

    Estrutura de selecao IF

---
+++ src/estrutura_if/ChecadorDeIdentidade.java
@@ -0,0 +1,23 @@
+
+package estrutura_if;
+
+import java.util.Scanner;
+
+public class ChecadorDeIdentidade {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe sua idade: ");
+        int idade = scanner.nextInt();
+
+        if (idade > 18) {
+            System.out.println("Pode comprar o ingresso.");
+        }else{
+            System.out.println("Esse filme é para maiores de 18 anos!");
+        }
+
+        scanner.close();
+    }
+}
