0c4a232c25c59a9bb2bddc33bcf47ba5d57a6f13
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-04-23 09:27:05 -0300

    Switch mais inxuto

--- src/estrutura_switch/ChecadorDeNotas.java
+++ src/estrutura_switch/ChecadorDeNotas.java
@@ -11,13 +11,14 @@ public class ChecadorDeNotas {
         System.out.println("Informe a nota (A, B, C, D, F)");
         String nota = console.readLine();
 
-        switch(nota){
-            case "A" -> System.out.println("Excelente!");
-            case "B" -> System.out.println("Bom trabalho!");
-            case "C" -> System.out.println("Está progredindo!");
-            case "D" -> System.out.println("Precisa melhorar!");
-            case "F" -> System.out.println("Você falhou!");
-            default -> System.out.println("Opção inválida.");
-        }
+       String mensagem = switch(nota){
+            case "A" -> "Excelente!";
+            case "B" -> "Bom trabalho!";
+            case "C" -> "Está progredindo!";
+            case "D" -> "Precisa melhorar!";
+            case "F" -> "Você falhou!";
+            default -> "Opção inválida."    ;   
+        };
+        System.out.println(mensagem);
     }
 }
